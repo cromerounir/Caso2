03a5ae4405c74fa8b20b51f365534b02fd2156cc
Carlos Romero <Carlos.Romero@king.com>
2022-03-04 11:46:48 +0100

    creando master-nfs separado

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "mySubnet" {
 
 }
 
-# Create NIC
+# Create NIC machines
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
 
 resource "azurerm_network_interface" "myNic1" {
@@ -46,12 +46,50 @@ resource "azurerm_network_interface" "myNic1" {
 
 }
 
-# IP pública
+# IP pública machines
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
 
 resource "azurerm_public_ip" "myPublicIp1" {
   name                = "vmip-${var.machines[count.index]}"
   count               = length(var.machines)
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Create NIC master
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_interface
+
+resource "azurerm_network_interface" "myNicmaster" {
+  name                = "masternic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+    name                           = "ipconf-master"
+    subnet_id                      = azurerm_subnet.mySubnet.id 
+    private_ip_address_allocation  = "Static"
+    private_ip_address             = "10.0.1.55"
+    public_ip_address_id           = azurerm_public_ip.myPublicIpmaster.id
+  }
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# IP pública machines
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/public_ip
+
+resource "azurerm_public_ip" "myPublicIpmaster" {
+  name                = "vmip-master"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -7,11 +7,23 @@ variable "location" {
 variable "vm_size" {
     type = string
     description = "VM size"
-    default = "Standard_D1_v2" # 3.5 GB, 1 CPU
+    default = "Standard_A1_v2" # 2 GB ram, 1 CPU, 10 GB Temporary Disk (SSD) 
 }
+
+variable "vm_size_master" {
+    type = string
+    description = "VM size"
+    default = "Standard_A2_v2" # 4 GB ram, 2 CPU, 20 GB Temporary Disk (SSD) 
+}
+
+variable "machine_master" {
+    type = string
+    description = "machines to create"
+    default = "master"
+    }
 
 variable "machines" {
     type = list(string)
     description = "machines to create"
-    default = ["master","worker01","worker02","nfsnode"]
+    default = ["worker01","worker02"]
 }
