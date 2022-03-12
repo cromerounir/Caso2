6ad75892347d733124da2485174fe5dd5145ec47
Carlos Romero <Carlos.Romero@king.com>
2022-03-12 16:01:02 +0100

    nueva prueba

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,7 +1,12 @@
 
 #!/bin/bash
 
-# añadir tantas líneas como sean necesarias para el correcto despligue
-
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i hosts -l XXXX playbook
+
+ansible-playbook -i hosts 1-previous-tasks.yaml
+ansible-playbook -i hosts 2-firewall-setup.yaml
+ansible-playbook -i hosts 3-nfs-config.yaml
+ansible-playbook -i hosts 4-k8s-installation.yaml
+ansible-playbook -i hosts 5-k8s-master.yaml
+ansible-playbook -i hosts 6-k8s-nodes.yaml
+ansible-playbook -i hosts 7-deploy-app.yam
